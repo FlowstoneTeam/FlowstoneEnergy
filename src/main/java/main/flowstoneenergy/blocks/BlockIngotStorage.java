0189d644e89897873f75b117e7a523d855436bbb
Tombenpotter <thomas@benguigui.fr>
2014-05-20 11:59:17 +0200

    Storage blocks now use metadata
    DUUUUUUUUUUH

---
+++ src/main/java/main/flowstoneenergy/blocks/BlockIngotStorage.java
@@ -0,0 +1,52 @@
+package main.flowstoneenergy.blocks;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import main.flowstoneenergy.FlowstoneEnergy;
+import main.flowstoneenergy.ModInfo;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
+import java.util.List;
+
+public class BlockIngotStorage extends Block {
+    public IIcon[] icon = new IIcon[16];
+
+    public BlockIngotStorage() {
+        super(Material.iron);
+        this.setHardness(0.5F);
+        this.setCreativeTab(FlowstoneEnergy.tab);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister ir) {
+        this.icon[0] = ir.registerIcon(ModInfo.MODID + ":blockedingots/blockCopper");
+        this.icon[1] = ir.registerIcon(ModInfo.MODID + ":blockedingots/blockTin");
+        this.icon[2] = ir.registerIcon(ModInfo.MODID + ":blockedingots/blockLead");
+        this.icon[3] = ir.registerIcon(ModInfo.MODID + ":blockedingots/blockSilver");
+        this.icon[4] = ir.registerIcon(ModInfo.MODID + ":blockedingots/blockEnder");
+        this.icon[5] = ir.registerIcon(ModInfo.MODID + ":blockedingots/blockNickel");
+        this.icon[6] = ir.registerIcon(ModInfo.MODID + ":blockedingots/blockInvar");
+        this.icon[7] = ir.registerIcon(ModInfo.MODID + ":blockedingots/blockElectrum");
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta) {
+        return this.icon[meta];
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item id, CreativeTabs tab, List list) {
+        for (int i = 0; i < 8; i++) {
+            list.add(new ItemStack(id, 1, i));
+        }
+    }
+}
---
+++ src/main/java/main/flowstoneenergy/items/blocks/ItemBlockIngotStorage.java
@@ -0,0 +1,59 @@
+package main.flowstoneenergy.items.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+
+public class ItemBlockIngotStorage extends ItemBlock {
+    public ItemBlockIngotStorage(Block block) {
+        super(block);
+        setHasSubtypes(true);
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemstack) {
+        String name = "";
+        switch (itemstack.getItemDamage()) {
+            case 0: {
+                name = "copper";
+                break;
+            }
+            case 1: {
+                name = "tin";
+                break;
+            }
+            case 2: {
+                name = "lead";
+                break;
+            }
+            case 3: {
+                name = "silver";
+                break;
+            }
+            case 4: {
+                name = "ender";
+                break;
+            }
+            case 5: {
+                name = "nickel";
+                break;
+            }
+            case 6: {
+                name = "invar";
+                break;
+            }
+            case 7: {
+                name = "electrum";
+                break;
+            }
+            default:
+                name = "nothing";
+        }
+        return getUnlocalizedName() + "." + name;
+    }
+
+    @Override
+    public int getMetadata(int par1) {
+        return par1;
+    }
+}
