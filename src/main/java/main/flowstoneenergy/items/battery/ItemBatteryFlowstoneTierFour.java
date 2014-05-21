97ffbac5847ecb2d810b8e90593f2c404ac0c469
poppypoppop <jimbo@exemail.com.au>
2014-05-21 21:41:21 +1000

    Update ItemBatteryFlowstoneTierFour.java

--- src/main/java/main/flowstoneenergy/items/battery/ItemBatteryFlowstoneTierFour.java
+++ src/main/java/main/flowstoneenergy/items/battery/ItemBatteryFlowstoneTierFour.java
@@ -3,10 +3,26 @@ package main.flowstoneenergy.items.battery;
 import main.flowstoneenergy.ModInfo;
 
 public class ItemBatteryFlowstoneTierFour extends ItemBatteryFlowstoneTierOne {
+	
+	private int maxFE = 100000;
+	public int currentFE = 0;
+	
 	public ItemBatteryFlowstoneTierFour() {
 		super();
 		this.setTextureName(ModInfo.MODID + ":batteries/tierFour");
 		this.setUnlocalizedName(ModInfo.MODID + ".flowstone.battery.tier.four");
 		this.setMaxStackSize(1);
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) {
+		list.add(TextHelper.shiftForMoreInfo);
+    	if (!KeyboardHelper.isShiftDown()) {
+			return;
+		}
+    	list.remove(1);
+		list.add(currentFE + "/" + maxFE + "FE Stored");
+		list.add("RIGHT CLICK to activate");
 	}
 }
