96021a9cda06773d7f34154148c206ce959c4dc7
poppypoppop <jimbo@exemail.com.au>
2014-05-21 21:44:10 +1000

    Update TextHelper.java

--- src/main/java/main/flowstoneenergy/utils/TextHelper.java
+++ src/main/java/main/flowstoneenergy/utils/TextHelper.java
@@ -3,32 +3,33 @@ package main.flowstoneenergy.utils;
 public class TextHelper {
 
 	/** Color Coding **/
-	public static String black = "�0";
-	public static String darkBlue = "�1";
-	public static String darkGreen = "�2";
-	public static String darkAqua = "�3";
-	public static String darkRed = "�4";
-	public static String darkPurple = "�5";
-	public static String gold = "�6";
-	public static String gray = "�7";
-	public static String darkGray = "�8";
-	public static String blue = "�9";
-	public static String green = "�a";
-	public static String aqua = "�b";
-	public static String red = "�c";
-	public static String lightPurple = "�d";
-	public static String yellow = "�e";
-	public static String white = "�f";
+	public static final String BLACK = (char) 167 + "0";
+	public static final String BLUE = (char) 167 + "1";
+	public static final String GREEN = (char) 167 + "2";
+	public static final String TEAL = (char) 167 + "3";
+	public static final String RED = (char) 167 + "4";
+	public static final String PURPLE = (char) 167 + "5";
+	public static final String ORANGE = (char) 167 + "6";
+	public static final String LIGHT_GRAY = (char) 167 + "7";
+	public static final String GRAY = (char) 167 + "8";
+	public static final String LIGHT_BLUE = (char) 167 + "9";
+	public static final String BRIGHT_GREEN = (char) 167 + "a";
+	public static final String BRIGHT_BLUE = (char) 167 + "b";
+	public static final String LIGHT_RED = (char) 167 + "c";
+	public static final String PINK = (char) 167 + "d";
+	public static final String YELLOW = (char) 167 + "e";
+	public static final String WHITE = (char) 167 + "f";
 
-	/** Text Formating **/
-	public static String obfuscated = "�k";
-	public static String bold = "�l";
-	public static String strikethrough = "�m";
-	public static String underline = "�n";
-	public static String italics = "�o";
-	public static String resetFormatting = "�r";
+	/** Text formatting **/
+	
+	public static final String OBFUSCATED = (char) 167 + "k";
+	public static final String BOLD = (char) 167 + "l";
+	public static final String STRIKETHROUGH = (char) 167 + "m";
+	public static final String UNDERLINE = (char) 167 + "n";
+	public static final String ITALIC = (char) 167 + "o";
+	public static final String END = (char) 167 + "r";
 
 	/** Useful Tooltips **/
-	public static String shiftForMoreInfo = darkGreen + italics + "Press Shift for more information";
+	public static String shiftForMoreInfo = ORANGE + ITALIC + "Press SHIFT for more information";
 
 }
