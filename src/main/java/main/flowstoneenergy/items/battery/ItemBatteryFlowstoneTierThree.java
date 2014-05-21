fb38191defd1ce873f05ba34515b745583f9001d
poppypoppop <jimbo@exemail.com.au>
2014-05-21 21:42:11 +1000

    Update ItemBatteryFlowstoneTierThree.java

--- src/main/java/main/flowstoneenergy/items/battery/ItemBatteryFlowstoneTierThree.java
+++ src/main/java/main/flowstoneenergy/items/battery/ItemBatteryFlowstoneTierThree.java
@@ -3,10 +3,25 @@ package main.flowstoneenergy.items.battery;
 import main.flowstoneenergy.ModInfo;
 
 public class ItemBatteryFlowstoneTierThree extends ItemBatteryFlowstoneTierOne {
+	private int maxFE = 10000;
+	public int currentFE = 0;
+	
 	public ItemBatteryFlowstoneTierThree() {
 		super();
 		this.setTextureName(ModInfo.MODID + ":batteries/tierThree");
 		this.setUnlocalizedName(ModInfo.MODID + ".flowstone.battery.tier.three");
 		this.setMaxStackSize(1);
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) {
+		list.add(TextHelper.shiftForMoreInfo);
+    	if (!KeyboardHelper.isShiftDown()) {
+			return;
+		}
+    	list.remove(1);
+		list.add(currentFE + "/" + maxFE + "FE Stored");
+		list.add("RIGHT CLICK to activate");
 	}
 }
