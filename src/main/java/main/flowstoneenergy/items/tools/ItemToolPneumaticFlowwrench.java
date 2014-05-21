a041c24718c466d76d258cc09f5a4ad00fd12e8b
poppypoppop <jimbo@exemail.com.au>
2014-05-21 21:43:29 +1000

    Update ItemToolPneumaticFlowwrench.java

--- src/main/java/main/flowstoneenergy/items/tools/ItemToolPneumaticFlowwrench.java
+++ src/main/java/main/flowstoneenergy/items/tools/ItemToolPneumaticFlowwrench.java
@@ -6,10 +6,23 @@ import main.flowstoneenergy.ModInfo;
 
 public class ItemToolPneumaticFlowwrench extends ItemToolFlowwrench {
 	Random rand = new Random();
+	private int maxFE = 10000;
+	public int currentFE = 0;
 
     public ItemToolPneumaticFlowwrench() {
         super();
         this.setUnlocalizedName(ModInfo.MODID + ".pneumatic.flowwrench");
         this.setTextureName(ModInfo.MODID + ":tools/pneumaticFlowwrench");
     }
+    
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) {
+    	list.add(TextHelper.shiftForMoreInfo);
+    	if (!KeyboardHelper.isShiftDown()) {
+			return;
+		}
+    	list.remove(1);
+		list.add(currentFE + "/" + maxFE + "FE Stored");
+	}
 }
