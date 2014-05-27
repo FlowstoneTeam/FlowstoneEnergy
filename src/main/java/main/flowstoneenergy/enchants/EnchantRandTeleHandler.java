2375c10f63d45e5adc41e6646bee6e0b19fa1067
poppypoppop <jimbo@exemail.com.au>
2014-05-27 18:05:35 +1000

    Update EnchantRandTeleHandler.java

--- src/main/java/main/flowstoneenergy/enchants/EnchantRandTeleHandler.java
+++ src/main/java/main/flowstoneenergy/enchants/EnchantRandTeleHandler.java
@@ -1,8 +1,6 @@
 package main.flowstoneenergy.enchants;
 
 import java.util.Random;
-
-import main.flowstoneenergy.FlowstoneEnergy;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -14,27 +12,27 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class EnchantRandTeleHandler {
 	@SubscribeEvent
 	public void RandomTeleport(LivingAttackEvent event) {
-		EntityPlayer attacker = event.source;
+		EntityPlayer attacker = (EntityPlayer) event.source.getEntity();
 		Entity defender = event.entity;
 		World world = event.entity.worldObj;
 		if(world.isRemote) return;
-        	if(event.source.getEntity() == null) return;
-        	if(!(event.source.getEntity() instanceof EntityPlayer)) return;
-        	if(attacker.getHeldItem() == null) return;
-        	if(!attacker.getHeldItem().isItemEnchanted());
-        	if(!(EnchantmentHelper.getEnchantments(attacker.getHeldItem()).containsKey(EnchantRegistry.randTeleEnchant.effectId))) return;
-        	teleport(world, event.source.getEntity());
+        if(event.source.getEntity() == null) return;
+        if(!(event.source.getEntity() instanceof EntityPlayer)) return;
+        if(attacker.getHeldItem() == null) return;
+        if(!attacker.getHeldItem().isItemEnchanted());
+        if(!(EnchantmentHelper.getEnchantments(attacker.getHeldItem()).containsKey(EnchantRegistry.randTeleEnchant.effectId))) return;
+        teleport(world, defender);
 	}
 	
 	public void teleport(World world, Entity entity) {
 		Random rand = new Random();
+		world.playSoundEffect(entity.posX + 0.5D, entity.posY + 0.5D, entity.posZ + 0.5D, "mob.endermen.portal", 1.0F, world.rand.nextFloat() * 0.1F + 0.9F);
 		if (rand.nextBoolean()) entity.posX += rand.nextDouble() + (rand.nextInt(5 - 0) + 0); else entity.posX -= rand.nextDouble() + (rand.nextInt(5 - 0) + 0);
 		entity.posY += 40;
 		if (rand.nextBoolean()) entity.posZ += rand.nextDouble() + (rand.nextInt(5 - 0) + 0); else entity.posZ -= rand.nextDouble() + (rand.nextInt(5 - 0) + 0);
 		while (world.getBlock((int)entity.posX, (int)entity.posY - 1, (int)entity.posZ) == Blocks.air) {
 			entity.posY -= 1;
 		}
-				
 		entity.setPosition(entity.posX, entity.posY, entity.posZ);
 	}
 }
