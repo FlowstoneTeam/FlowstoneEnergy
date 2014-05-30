1e50523d287c4e30c6f947f6f307fd8dcc31668f
poppypoppop <jimbo@exemail.com.au>
2014-05-30 17:51:05 +1000

    Update BlockRecipeRegistry.java

--- src/main/java/main/flowstoneenergy/blocks/BlockRecipeRegistry.java
+++ src/main/java/main/flowstoneenergy/blocks/BlockRecipeRegistry.java
@@ -3,12 +3,14 @@ package main.flowstoneenergy.blocks;
 import cpw.mods.fml.common.registry.GameRegistry;
 import main.flowstoneenergy.items.ItemRegistry;
 import main.flowstoneenergy.tileentities.recipes.RecipesMachineWorkbench;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 
 public class BlockRecipeRegistry {
 
     private static void registerBlockRecipes() {
         GameRegistry.addRecipe(new ItemStack(BlockRegistry.machineBox), "III", "I I", "III", 'I', new ItemStack(ItemRegistry.metaResourceIngot, 1, 0));
+        GameRegistry.addRecipe(new ItemStack(BlockRegistry.machines, 1, 5), "GCG", "CMC", "GCG", 'G', new ItemStack(ItemRegistry.metaResourceGear, 1, 1), 'C', Blocks.crafting_table, 'M', new ItemStack(BlockRegistry.machineBox));
     }
 
     private static void registerStorageRecipes() {
@@ -24,6 +26,10 @@ public class BlockRecipeRegistry {
 
     private static void registerWorkbenchRecipes() {
         RecipesMachineWorkbench.addRecipe(new ItemStack(BlockRegistry.machineBox), new ItemStack(ItemRegistry.metaResourceGear, 1, 2), new ItemStack(ItemRegistry.metaResourceGear, 1, 2), new ItemStack(BlockRegistry.machines, 1, 3), 200);
+        RecipesMachineWorkbench.addRecipe(new ItemStack(BlockRegistry.machineBox), new ItemStack(ItemRegistry.metaResourceGear, 1, 1), new ItemStack(ItemRegistry.metaResourceGear, 1, 2), new ItemStack(BlockRegistry.machines, 1, 2), 200);
+        RecipesMachineWorkbench.addRecipe(new ItemStack(BlockRegistry.machineBox), new ItemStack(ItemRegistry.metaResourceGear, 1, 2), new ItemStack(ItemRegistry.metaResourceGear, 1, 3), new ItemStack(BlockRegistry.machines, 1, 0), 200);
+        RecipesMachineWorkbench.addRecipe(new ItemStack(BlockRegistry.machineBox), new ItemStack(ItemRegistry.metaResourceGear, 1, 4), new ItemStack(ItemRegistry.metaResourceGear, 1, 3), new ItemStack(BlockRegistry.machines, 1, 1), 200);
+        RecipesMachineWorkbench.addRecipe(new ItemStack(BlockRegistry.machineBox), new ItemStack(ItemRegistry.metaResourceGear, 1, 13), new ItemStack(ItemRegistry.metaResourceGear, 1, 13), new ItemStack(BlockRegistry.machines, 1, 4), 200);
     }
 
     public static void registerFullRecipes() {
