3a895b346624335b872d4919843d0e6188a3979f
poppypoppop <jimbo@exemail.com.au>
2014-05-31 13:05:56 +1000

    Update FEEntityRegistry.java

--- src/main/java/main/flowstoneenergy/entities/FEEntityRegistry.java
+++ src/main/java/main/flowstoneenergy/entities/FEEntityRegistry.java
@@ -1,5 +1,7 @@
 package main.flowstoneenergy.entities;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import main.flowstoneenergy.FlowstoneEnergy;
 
@@ -7,6 +9,27 @@ public class FEEntityRegistry {
 
     public static void init() {
         EntityRegistry.registerModEntity(EntityRobot.class, "EntityRobot", 0, FlowstoneEnergy.instance, 80, 3, true);
+        
+        registerEntityEgg(EntityRobot.class, 0 ,0);
     }
 
+    @SuppressWarnings("unchecked")
+    public static void registerEntityEgg(Class <? extends Entity> entity, int primaryColor, int secondaryColor)
+    {
+        int id = getUniqueEntityId();
+        EntityList.IDtoClassMapping.put(Integer.valueOf(id), entity);
+        EntityList.entityEggs.put(Integer.valueOf(id), new EntityList.EntityEggInfo(id, primaryColor, secondaryColor));
+    }
+        
+    public static int getUniqueEntityId()
+    {
+    	int startEntityId = 400;
+        do
+        {
+            ++startEntityId ;
+        }
+        while (EntityList.getStringFromID(startEntityId) != null);
+
+        return startEntityId;
+    }
 }
