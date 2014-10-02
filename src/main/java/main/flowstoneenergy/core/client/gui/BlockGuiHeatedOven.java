e87801c134b547c821ee71c035851c75ed327a23
Glassmaker <JeanGlassmaker@mail.com>
2014-10-02 04:31:15 +0300

    Adds power level in the GUI for the Bottler, Oven, and Mill

--- src/main/java/main/flowstoneenergy/core/client/gui/BlockGuiHeatedOven.java
+++ src/main/java/main/flowstoneenergy/core/client/gui/BlockGuiHeatedOven.java
@@ -1,5 +1,7 @@
 package main.flowstoneenergy.core.client.gui;
 
+import java.util.ArrayList;
+
 import main.flowstoneenergy.core.libs.ModInfo;
 import main.flowstoneenergy.core.containers.ContainerMachineHeatedOven;
 import main.flowstoneenergy.tileentities.machines.TileEntityMachineHeatedOven;
@@ -8,6 +10,8 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
+import net.minecraftforge.common.util.ForgeDirection;
+
 import org.lwjgl.opengl.GL11;
 
 public class BlockGuiHeatedOven extends GuiContainer {
@@ -43,5 +47,27 @@ public class BlockGuiHeatedOven extends GuiContainer {
 
         int i1 = this.te.getScaledProgress(24);
         this.drawTexturedModalRect(xStart + 81, yStart + 34, 176, 14, i1 + 1, 16);
+
+        int powerBarHeight = this.te.getEnergyStored(ForgeDirection.UNKNOWN) * 50 / this.te.getMaxEnergyStored(ForgeDirection.UNKNOWN);
+        int powerBarY = yStart + 10 + 50 - powerBarHeight;
+        this.drawTexturedModalRect(xStart + 12, powerBarY, 177, 32, 12, powerBarHeight + 1);
     }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float mouseAngle) {
+        super.drawScreen(mouseX, mouseY, mouseAngle);
+
+        int xStart = (width - xSize) / 2;
+        int yStart = (height - ySize) / 2;
+
+        if ((xStart + 12 <= mouseX) && (xStart + 24 > mouseX) && (yStart + 10 <= mouseY) && (yStart + 60 > mouseY)) {
+            String energyStatus = "Energy: ";
+            energyStatus += this.te.getEnergyStored(ForgeDirection.UNKNOWN) + " / " + this.te.getMaxEnergyStored(ForgeDirection.UNKNOWN);
+            energyStatus += " RF";
+            ArrayList<String> toolTipList = new ArrayList<String>();
+            toolTipList.add(energyStatus);
+            drawHoveringText(toolTipList, mouseX, mouseY, fontRendererObj);
+        }
+    }
+
 }
--- src/main/java/main/flowstoneenergy/core/client/gui/BlockGuiLumberMill.java
+++ src/main/java/main/flowstoneenergy/core/client/gui/BlockGuiLumberMill.java
@@ -1,5 +1,8 @@
 package main.flowstoneenergy.core.client.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import main.flowstoneenergy.tileentities.machines.TileEntityMachineLumberMill;
 import main.flowstoneenergy.core.libs.ModInfo;
 import main.flowstoneenergy.core.containers.ContainerMachineLumberMill;
@@ -8,6 +11,8 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
+import net.minecraftforge.common.util.ForgeDirection;
+
 import org.lwjgl.opengl.GL11;
 
 public class BlockGuiLumberMill extends GuiContainer {
@@ -43,5 +48,26 @@ public class BlockGuiLumberMill extends GuiContainer {
 
         int i1 = this.te.getScaledProgress(24);
         this.drawTexturedModalRect(xStart + 81, yStart + 34, 176, 14, i1 + 1, 16);
+
+        int powerBarHeight = this.te.getEnergyStored(ForgeDirection.UNKNOWN) * 50 / this.te.getMaxEnergyStored(ForgeDirection.UNKNOWN);
+        int powerBarY = yStart + 10 + 50 - powerBarHeight;
+        this.drawTexturedModalRect(xStart + 12, powerBarY, 177, 32, 12, powerBarHeight + 1);
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float mouseAngle) {
+        super.drawScreen(mouseX, mouseY, mouseAngle);
+
+        int xStart = (width - xSize) / 2;
+        int yStart = (height - ySize) / 2;
+
+        if ((xStart + 12 <= mouseX) && (xStart + 24 > mouseX) && (yStart + 10 <= mouseY) && (yStart + 60 > mouseY)) {
+            String energyStatus = "Energy: ";
+            energyStatus += this.te.getEnergyStored(ForgeDirection.UNKNOWN) + " / " + this.te.getMaxEnergyStored(ForgeDirection.UNKNOWN);
+            energyStatus += " RF";
+            ArrayList<String> toolTipList = new ArrayList<String>();
+            toolTipList.add(energyStatus);
+            drawHoveringText(toolTipList, mouseX, mouseY, fontRendererObj);
+        }
     }
 }
