a4807156e82f304554cf1c20a01e474a765509be
Glassmaker <JeanGlassmaker@mail.com>
2014-10-15 16:25:31 +0300

    Implemented RF compat in batteries

--- src/main/java/main/flowstoneenergy/items/battery/ItemBatteryFlowstoneBase.java
+++ src/main/java/main/flowstoneenergy/items/battery/ItemBatteryFlowstoneBase.java
@@ -2,6 +2,7 @@ package main.flowstoneenergy.items.battery;
 
 import java.util.List;
 
+import cofh.api.energy.IEnergyContainerItem;
 import main.flowstoneenergy.FlowstoneEnergy;
 import main.flowstoneenergy.core.utils.KeyboardHelper;
 import main.flowstoneenergy.core.utils.TextHelper;
@@ -13,10 +14,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
-public class ItemBatteryFlowstoneBase extends Item {
+public class ItemBatteryFlowstoneBase extends Item implements IEnergyContainerItem {
 
     private int maxPower = 0;
     private int currentPower = 0;
+    private int powerRate = 50;
 
     public ItemBatteryFlowstoneBase() {
         super();
@@ -42,7 +44,6 @@ public class ItemBatteryFlowstoneBase extends Item {
      * @return max power amount.
      */
     public int getMaxPower(ItemStack stack) {
-        readNBTTags(stack);
         return this.maxPower;
     }
 
@@ -57,15 +58,59 @@ public class ItemBatteryFlowstoneBase extends Item {
     }
 
     /**
+     * Sets the battery's current power charge.
+     * 
+     * @param stack
+     *            Battery ItemStack.
+     * @param currentPower
+     *            current power charge amount.
+     */
+    public void setCurrentPower(ItemStack stack, int currentPower) {
+
+        if (stack == null || stack.getItem() == null)
+            return;
+
+        if (!(stack.getItem() instanceof ItemBatteryFlowstoneBase))
+            return;
+
+        if (currentPower < 0)
+            currentPower = 0;
+
+        if (currentPower > maxPower)
+            currentPower = maxPower;
+
+        NBTTagCompound compound = stack.getTagCompound();
+        if (compound == null)
+            compound = new NBTTagCompound();
+
+        NBTTagCompound powerCompound = compound.getCompoundTag("flowstonePower");
+        if (powerCompound == null)
+            powerCompound = new NBTTagCompound();
+
+        powerCompound.setInteger("currentPower", currentPower);
+
+        compound.setTag("flowstonePower", powerCompound);
+
+        stack.setTagCompound(compound);
+    }
+
+    /**
      * Gets the battery's current power.
      * 
      * @param stack
      *            the ItemStack to retrieve the current power from.
      * @return current power charge amount.
      */
-    protected int getCurrentPower(ItemStack stack) {
-        readNBTTags(stack);
-        return this.currentPower;
+    public int getCurrentPower(ItemStack stack) {
+        NBTTagCompound compound = stack.getTagCompound();
+        if (compound == null)
+            return 0;
+
+        NBTTagCompound powerCompound = compound.getCompoundTag("flowstonePower");
+        if (powerCompound == null)
+            return 0;
+
+        return powerCompound.getInteger("currentPower");
     }
 
     /**
@@ -133,9 +178,10 @@ public class ItemBatteryFlowstoneBase extends Item {
         if (!KeyboardHelper.isShiftDown()) {
             list.add(TextHelper.shiftForMoreInfo);
         } else {
-            if (stack.getTagCompound() != null && stack.getTagCompound().getCompoundTag("flowstonePower") != null)
+            if (stack.getTagCompound() != null && stack.getTagCompound().getCompoundTag("flowstonePower") != null) {
                 list.add(TextHelper.GREEN + this.getCurrentPower(stack) + "/" + this.getMaxPower(stack) + TextHelper.localize("info.fe.tooltip.stored"));
-            else
+
+            } else
                 list.add(TextHelper.GREEN + this.currentPower + "/" + this.maxPower + TextHelper.localize("info.fe.tooltip.stored"));
             list.add(TextHelper.localize("info.fe.tooltip.use") + " " + TextHelper.localize("info.fe.control.sneak") + " " + TextHelper.localize("info.fe.tooltip.activate"));
         }
@@ -169,4 +215,70 @@ public class ItemBatteryFlowstoneBase extends Item {
 
         stack.setTagCompound(compound);
     }
+
+    /* ************** Start IEnergyContainerItem implementation ************** */
+
+    @Override
+    public int receiveEnergy(ItemStack container, int maxReceive, boolean simulate) {
+
+        if (container == null || container.getItem() == null)
+            return 0;
+
+        if (!(container.getItem() instanceof ItemBatteryFlowstoneBase))
+            return 0;
+
+        int received = maxReceive;
+
+        if (received > powerRate)
+            received = powerRate;
+
+        int currentStackPower = getCurrentPower(container);
+
+        if ((received + currentStackPower) >= maxPower)
+            received = maxPower - currentStackPower;
+
+        if (!simulate) {
+            setCurrentPower(container, received + currentStackPower);
+        }
+
+        return received;
+    }
+
+    @Override
+    public int extractEnergy(ItemStack container, int maxExtract, boolean simulate) {
+
+        if (container == null || container.getItem() == null)
+            return 0;
+
+        if (!(container.getItem() instanceof ItemBatteryFlowstoneBase))
+            return 0;
+
+        int extract = maxExtract;
+
+        if (extract > powerRate)
+            extract = powerRate;
+
+        int currentStackPower = getCurrentPower(container);
+
+        if ((currentStackPower - extract) < 0)
+            extract = currentStackPower;
+
+        if (!simulate) {
+            setCurrentPower(container, currentStackPower - extract);
+        }
+
+        return extract;
+    }
+
+    @Override
+    public int getEnergyStored(ItemStack container) {
+        return getCurrentPower(container);
+    }
+
+    @Override
+    public int getMaxEnergyStored(ItemStack container) {
+        return getMaxPower(container);
+    }
+
+    /* ************** End IEnergyContainerItem implementation ************** */
 }
