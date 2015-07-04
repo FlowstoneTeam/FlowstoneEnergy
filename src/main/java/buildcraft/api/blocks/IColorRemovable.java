a0b8585490955d40bb957d4edb30e2e47856beb7
Glassmaker <JeanGlassmaker@mail.com>
2015-07-05 02:33:04 +0300

    Moves Buildcraft's api, to be not packaged

--- src/main/java/buildcraft/api/blocks/IColorRemovable.java
+++
@@ -1,8 +0,0 @@
-package buildcraft.api.blocks;
-
-import net.minecraft.world.World;
-import net.minecraftforge.common.util.ForgeDirection;
-
-public interface IColorRemovable {
-	boolean removeColorFromBlock(World world, int x, int y, int z, ForgeDirection side);
-}
