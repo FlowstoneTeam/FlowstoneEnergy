c7c844f7873b74b862de8aee2337a6802805a819
Glassmaker <JeanGlassmaker@mail.com>
2015-11-28 12:23:13 +0300

    Adds insert for IAlternatingAppliance

--- src/main/java/main/flowstoneenergy/api/energy/alternating/IAlternatingAppliance.java
+++ src/main/java/main/flowstoneenergy/api/energy/alternating/IAlternatingAppliance.java
@@ -1,5 +1,8 @@
 package main.flowstoneenergy.api.energy.alternating;
 
+import net.minecraft.util.EnumFacing;
+
 public interface IAlternatingAppliance {
 
+    int insert(int flow, EnumAlternatingPhase phase, EnumFacing side, boolean real);
 }
